f8742845e319ed0cd37fc1c19a95c3b2a1cc1ccb
Mdominykas <marmadominykas@gmail.com>
2021-09-21 17:15:04 +0300

    added location list

--- Encounter1/MenuActivity.cs
+++ Encounter1/MenuActivity.cs
@@ -14,12 +14,20 @@ namespace Encounter1
     [Activity(Label = "MenuActivity")]
     public class MenuActivity : Activity
     {
+        Button btnLocationList;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Menu);
             TextView currentCharacterName = FindViewById<TextView>(Resource.Id.textViewUserName);
             currentCharacterName.Text = Intent.Extras.GetString("userName");
+            btnLocationList = FindViewById<Button>(Resource.Id.button1);
+            btnLocationList.Click += OnLocationListButtonClicked;
+        }
+
+        private void OnLocationListButtonClicked(object sender, EventArgs e)
+        {
+            StartActivity(typeof(LocationListActivity));
         }
     }
 }
