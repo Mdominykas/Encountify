3cbe6d7d5e8f1c78570f3470e4cee82d410b041e
urtė <73136801+hickoryeyes@users.noreply.github.com>
2021-09-28 15:23:35 +0300

    Simplified Shell

--- Encountify/ViewModels/RegisterViewModel.cs
+++ Encountify/ViewModels/RegisterViewModel.cs
@@ -8,17 +8,5 @@ namespace Encountify.ViewModels
 {
     public class RegisterViewModel : BaseViewModel
     {
-        public Command RegisterCommand { get; }
-
-        public RegisterViewModel()
-        {
-            RegisterCommand = new Command(OnRegisterClicked);
-        }
-
-        private async void OnRegisterClicked(object obj)
-        {
-            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
-        }
     }
 }
