7f9d75d6d36bc3bb34e23d73488e6f4902426311
urtė <73136801+hickoryeyes@users.noreply.github.com>
2021-09-28 17:52:17 +0300

    Added ability to show or hide password

---
+++ Encountify/CustomRenderer/ShowPasswordTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Encountify.CustomRenderer
+{
+    class ShowPasswordTrigger : TriggerAction<Button>, INotifyPropertyChanged
+    {
+        public string ShowIcon { get; set; }
+        public string HideIcon { get; set; }
+
+        string _icon;
+        bool _hidePassword = true;
+
+        public bool HidePassword
+        {
+            get => _hidePassword;
+            set
+            {
+                if (_hidePassword != value)
+                {
+                    _hidePassword = value;
+
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HidePassword)));
+                }
+            }
+        }
+
+        protected override void Invoke(Button sender)
+        {
+            sender.Text = HidePassword ? ShowIcon : HideIcon;
+            HidePassword = !HidePassword;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+    }
+}
