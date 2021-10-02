e3c9b76b0d32b2c11440ad5fb92d830a1b55ed48
Waltzerizm <54909838+Waltzerizm@users.noreply.github.com>
2021-10-02 14:12:25 +0300

    Reverted some changes on the LoadDummyData() method

--- Encountify/Services/MockDataStore.cs
+++ Encountify/Services/MockDataStore.cs
@@ -57,25 +57,13 @@ namespace Encountify.Services
 
         public void LoadDummyData(SQLiteConnection db)
         {
-            if (db.Table<Location>().Count() <= 7)
+            if (db.Table<Location>().Count() == 0)
             {
                 var location1 = new Location() { Name = "Vilniaus katedra", CoordX = 54.685849042698216, CoordY = 25.287750880122083 };
                 var location2 = new Location() { Name = "Gedimino bokštas", CoordX = 54.68667445192699, CoordY = 25.29056883194689 };
-                var location3 = new Location() { Name = "Vilniaus Šv. Onos bažnyčia", CoordX = 54.68378573230062, CoordY = 25.292650881640785 };
-                var location4 = new Location() { Name = "Trys kryžiai", CoordX = 54.68740766559662, CoordY = 25.29771489238211 };
-                var location5 = new Location() { Name = "Gedimino prospektas", CoordX = 54.68644019450281, CoordY = 25.285441103636185 };
-                var location6 = new Location() { Name = "Lietuvos Respublikos Prezidento kanceliarija", CoordX = 54.68383535000863, CoordY = 25.286685648648888 };
-                var location7 = new Location() { Name = "Sereikiškių parko Bernardinų sodas", CoordX = 54.68413305498285, CoordY = 25.29522580235671 };
-                var location8 = new Location() { Name = "Lietuvos nacionalinis dailės muziejus", CoordX = 54.68130476957157, CoordY = 25.289818468853266 };
 
                 db.Insert(location1);
                 db.Insert(location2);
-                db.Insert(location3);
-                db.Insert(location4);
-                db.Insert(location5);
-                db.Insert(location6);
-                db.Insert(location7);
-                db.Insert(location8);
             }
         }
 
