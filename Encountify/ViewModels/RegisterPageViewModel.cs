8adfed66c76721e00d62e9424817f5b859b95d93
Mdominykas <50084966+Mdominykas@users.noreply.github.com>
2021-10-12 16:12:03 +0300

    Service terms (#50)

---
+++ Encountify/ViewModels/RegisterPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Encountify.ViewModels
+{
+    class RegisterPageViewModel : BaseViewModel
+    {
+        public ICommand ReadServiceTermsCommand => new Command(OnReadServiceTerms);
+        public Command CancelCommand { get; }
+
+        public RegisterPageViewModel()
+        {
+            CancelCommand = new Command(OnCancel);
+        }
+
+        private async void OnCancel()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+
+        private async void OnReadServiceTerms()
+        {
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(LoadResourceText)).Assembly;
+            Stream stream = assembly.GetManifestResourceStream("Encountify.ServiceTerms.txt");
+            string terms = "";
+            using (var reader = new StreamReader(stream))
+            {
+                terms = reader.ReadToEnd();
+            }
+            await App.Current.MainPage.DisplayAlert("Service terms", terms, "OK");
+        }
+
+    }
+}
