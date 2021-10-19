5b08cba8c88d645f04908591ddc8fb522549be2b
Mdominykas <50084966+Mdominykas@users.noreply.github.com>
2021-10-19 18:54:22 +0300

    added scoreboard (#58)

---
+++ Encountify/ViewModels/ScoreboardPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Encountify.Models;
+using Encountify.Services;
+using Encountify.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Encountify.ViewModels
+{
+    public class ScoreboardPageViewModel : BaseViewModel
+    {
+        public ObservableCollection<ScoreboardEntry> Scoreboard { get; }
+
+        public ScoreboardPageViewModel()
+        {
+            Scoreboard = new ObservableCollection<ScoreboardEntry>();
+            var scoreboardCreator = new ScoreboardCreation();
+            var list = scoreboardCreator.CreateScoreboard();
+
+            foreach (var element in list)
+                Scoreboard.Add(element);
+        }
+    }
+}
