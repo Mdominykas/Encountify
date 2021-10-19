514655a70d7397d45685b63f7eead96741d53a28
MigleMic <90243853+MigleMic@users.noreply.github.com>
2021-10-19 20:20:34 +0300

    Update ListExtensions.cs

--- Encountify/ListExtensions.cs
+++ Encountify/ListExtensions.cs
@@ -6,7 +6,7 @@ namespace Encountify
 {
     public static class ListExtensions
     {
-        public static void SortdescendingOrder<T>(this List<T> list)
+        public static void SortDescendingOrder<T>(this List<T> list)
         {
             list.Sort();
             list.Reverse();
