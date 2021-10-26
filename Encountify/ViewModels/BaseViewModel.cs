fe5f22b252169de1cc5e4d25e68ab569933abac6
urtė <73136801+hickoryeyes@users.noreply.github.com>
2021-10-26 20:48:58 +0300

    Simplify BaseViewModel

--- Encountify/ViewModels/BaseViewModel.cs
+++ Encountify/ViewModels/BaseViewModel.cs
@@ -76,16 +76,10 @@ namespace Encountify.ViewModels
             }
         }
 
-        #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
-            var changed = PropertyChanged;
-            if (changed == null)
-                return;
-
-            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-        #endregion
     }
 }
