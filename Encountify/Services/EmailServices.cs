a388477ea6c37e95cd950d6bae71040b4a07be16
urtė <73136801+hickoryeyes@users.noreply.github.com>
2021-10-27 14:17:42 +0300

    Implement EmailServices class

---
+++ Encountify/Services/EmailServices.cs
@@ -0,0 +1,46 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using System.Threading.Tasks;
+
+namespace Encountify.Services
+{
+    public class EmailServices
+    {
+        // TODO: Get data from .env or somewhere else instead of writing it here
+        private const string Host = "smtp.gmail.com";
+        private const int Port = 465;
+        private const string AuthorName = "Encountify";
+        private const string AuthorEmail = "encountify@gmail.com";
+        private const string AuthorPassword = "Encountify123.";
+        private const SecureSocketOptions Ssl = SecureSocketOptions.SslOnConnect;
+
+        public MimeMessage Message(string RecipientName, string RecipientEmail, string Subject, string Content)
+        {
+            MimeMessage message = new MimeMessage();
+            message.From.Add(new MailboxAddress(AuthorName, AuthorEmail));
+            message.To.Add(new MailboxAddress(RecipientName, RecipientEmail));
+            message.Subject = Subject;
+            BodyBuilder bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = Content;
+            message.Body = bodyBuilder.ToMessageBody();
+            return message;
+        }
+
+        public async Task SendEmail(string Content, string RecipientName = AuthorName, string RecipientEmail = AuthorEmail, string Subject = "Untitled")
+        {
+            MimeMessage message = Message(RecipientName, RecipientEmail, Subject, Content);
+
+            using (var client = new SmtpClient())
+            {
+                client.CheckCertificateRevocation = false;
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
+                await client.ConnectAsync(Host, Port, Ssl);
+                await client.AuthenticateAsync(userName: AuthorEmail, password: AuthorPassword);
+                await client.SendAsync(message);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
--- Encountify/Views/ForgotPasswordPage.xaml.cs
+++ Encountify/Views/ForgotPasswordPage.xaml.cs
@@ -3,9 +3,6 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Encountify.Models;
 using Encountify.Services;
-using MimeKit;
-using MailKit.Net.Smtp;
-using MailKit.Security;
 using System.Linq;
 
 namespace Encountify.Views
@@ -33,36 +30,17 @@ namespace Encountify.Views
             var user = users.Where(x => x.Email == Email.Text).FirstOrDefault();
             if (user != null)
             {
-                SendEmail(user);
+                var services = new EmailServices();
+                await services.SendEmail(
+                    RecipientName: user.Username, 
+                    RecipientEmail: user.Email,
+                    Subject: "Forgot password?",
+                    Content: $"<b>Your username:</b> {user.Username}<br><b>Your password:</b> {user.Password}");
                 await DisplayAlert("Success!", $"Email was sent to {Email.Text}", "OK");
             }
             else
             {
                 await DisplayAlert("Warning", "No such email", "OK");
-            }
-        }
-
-        private async void SendEmail(User user)
-        {
-            var mailMessage = new MimeMessage();
-            mailMessage.From.Add(new MailboxAddress("Encountify", "encountify@gmail.com"));
-            mailMessage.To.Add(new MailboxAddress(user.Username, Email.Text));
-            mailMessage.Subject = "Forgot password?";
-            BodyBuilder bodyBuilder = new BodyBuilder();
-            bodyBuilder.HtmlBody = $"<b>Your username:</b> {user.Username}<br><b>Your password:</b> {user.Password}";
-            mailMessage.Body = bodyBuilder.ToMessageBody();
-
-            // TODO: Get data from .env instead of writing it here
-            // TODO: Move to a separate class
-            using (var client = new SmtpClient())
-            {
-                client.CheckCertificateRevocation = false;
-                client.AuthenticationMechanisms.Remove("XOAUTH2");
-                client.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
-                client.Connect("smtp.gmail.com", 465, SecureSocketOptions.SslOnConnect);
-                client.Authenticate("encountify@gmail.com", "Encountify123.");
-                await client.SendAsync(mailMessage);
-                client.Disconnect(true);
             }
         }
 
