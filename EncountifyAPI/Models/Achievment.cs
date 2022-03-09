363727d667c51e3bfb55909ccbf6dca172d7faa3
arma155 <78311210+ArmantasPik@users.noreply.github.com>
2022-03-10 01:23:16 +0200

    Add achievment data models

---
+++ EncountifyAPI/Models/Achievment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EncountifyAPI.Models
+{
+    public class Achievment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Category { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ EncountifyAPI/Models/AssignedAchievment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EncountifyAPI.Models
+{
+    public class AssignedAchievment
+    {
+        public int UserId { get; set; }
+        public int AchievmentId { get; set; }
+        public DateTime AssignmentDate { get; set; }
+    }
+}
